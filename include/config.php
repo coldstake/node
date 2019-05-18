629733cf766399a1b6d1f0a944d2f2d0a9ee1f2b
Hunter <thecryptohunter@protonmail.com>
2019-05-18 10:49:55 +0100

    Update

--- include/config.php
+++ include/config.php
@@ -8,9 +8,9 @@ $server_ip='localhost'; // '0.0.0.0' target server ip. [ex.] 10.0.0.15
 $scheme='http' ;// tcp protocol to access json on coin. [default]
 
 // Service specific variables.
-$service_desc='';
-$price='';
-$redirectURL='';
+$service_desc='12 months coldstake.co.in service';
+$price='15.00';
+$redirectURL='http://bitcoinc.coldstake.co.in/activate.php';
 $email='';
 
 // BTCPayServer variables
