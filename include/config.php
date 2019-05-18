7007bc8b9c78e2b5bde5f961ee4b51dd32a72e86
Hunter <thecryptohunter@protonmail.com>
2019-05-18 15:19:33 +0100

    Updates

--- include/config.php
+++ include/config.php
@@ -1,19 +1,19 @@
 <?php 
-// Coin specific variables - TODO: Inject these variables
-$ticker='bitcoinc';
-$server_port='9790';
-$rpc_user='Adi5Yvbv489TldEn4K6qgzBPfsnTzJEl';
-$rpc_pass='s1M9PxRzBw9zQhkhFktA6te5zcJOO0Kf';
-$server_ip='localhost'; // '0.0.0.0' target server ip. [ex.] 10.0.0.15
-$scheme='http' ;// tcp protocol to access json on coin. [default]
-
-// Service specific variables.
-$service_desc='12 months coldstake.co.in service';
-$price='15.00';
-$redirectURL='http://bitcoinc.coldstake.co.in/activate.php';
+// Coin specific variables will get injected
+$ticker='';
+$server_port='';
+$rpc_user='';
+$rpc_pass='';
+// Service specific variables will get injected
+$service_desc='';
+$price='';
+$redirectURL='';
 $email='';
 
 // BTCPayServer variables
 $encryt_pass='YourTopSecretPassword';
 
+// standard variables
+$server_ip='localhost'; // '0.0.0.0' target server ip. [ex.] 10.0.0.15
+$scheme='http' ;// tcp protocol to access json on coin. [default]
 ?>
