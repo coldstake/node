c1ec7b3ece8b2fa6565a8e395eb47664fdad89bf
Hunter <thecryptohunter@protonmail.com>
2019-05-18 10:37:11 +0100

    Update

--- include/config.php
+++ include/config.php
@@ -2,8 +2,8 @@
 // Coin specific variables - TODO: Inject these variables
 $ticker='bitcoinc';
 $server_port='9790';
-$rpc_user='jkrteenbEG1BE1Vlxv1ExABfLeHqbbyN';
-$rpc_pass='j3OVIJYDmTEQ8T0snHEP4Wfy2C6qvKlA';
+$rpc_user='Adi5Yvbv489TldEn4K6qgzBPfsnTzJEl';
+$rpc_pass='s1M9PxRzBw9zQhkhFktA6te5zcJOO0Kf';
 $server_ip='localhost'; // '0.0.0.0' target server ip. [ex.] 10.0.0.15
 $scheme='http' ;// tcp protocol to access json on coin. [default]
 
