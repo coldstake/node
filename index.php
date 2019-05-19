251b1e864928cfad79a4fbf0abb7e2910e18aa34
Hunter <thecryptohunter@protonmail.com>
2019-05-19 13:01:27 +0100

    update

--- index.php
+++ index.php
@@ -59,6 +59,7 @@ EOD;
 									<div id="menu">
 										<ul>
 											<li><a href="index.php">Home</a></li>
+											<li><a href="about.html">About</a></li>
 											<li><a href="landing.php">Order</a></li>
 										</ul>
 									</div>
@@ -75,7 +76,46 @@ EOD;
 							cold staking<br />
 							<a href="landing.php" class="more scrolly"><b>PAYMENT</b></a>
 					</section>
+				<!-- One -->
+				<section id="one" class="wrapper style1 special">
+						<div class="inner">
+							<header class="major">
+								<h2>Full Node as a Service</h2>
+								<p>Effortless cold staking with no technical knowledge required</p>
+							</header>
+							<ul class="icons major">
+								<li><span class="icon fa-diamond major style1"><span class="label">Lorem</span></span></li>
+								<li><span class="icon fa-heart-o major style2"><span class="label">Ipsum</span></span></li>
+								<li><span class="icon fa-code major style3"><span class="label">Dolor</span></span></li>
+							</ul>
+						</div>
+					</section>
 
+				<!-- Two -->
+					<section id="two" class="wrapper alt style2">
+						<section class="spotlight">
+							<div class="image"><img src="images/pic01.jpg" alt="" /></div><div class="content">
+								<h2>Cold Staking</h2>
+								<p>
+								Cold Staking lets users securely delegate staking powers to “staking nodes” which contain no coins. The purpose of these “staking nodes” is to provide a dedicated resource connected to a blockchain network and stake on behalf of another wallet without being able to spend its coins. In other words, it allows users to stake offline coins.
+								</p>
+							</div>
+						</section>
+						<section class="spotlight">
+							<div class="image"><img src="images/pic02.jpg" alt="" /></div><div class="content">
+								<h2>Benefits of staking</h2>
+								<p>Staking enables coin holders to earn compounding rewards in return for freezing their staked coins so they cannot be otherwise used while they are being staked. Stack those coins!</p>
+							</div>
+						</section>
+						<section class="spotlight">
+							<div class="image"><img src="images/pic03.jpg" alt="" /></div><div class="content">
+								<h2>Withdraw at anytime<br />
+								No Penalties</h2>
+								<p>Staked Coins aren't kept on the full node, so you and only you can withdraw your coins back to your wallet at anytime. No fee's or penalties.</p>
+							</div>
+						</section>
+					</section>
+					
 				<!-- Footer -->
 					<footer id="footer">
 						<ul class="icons">
