88b76faf79ae182b181e427d20bc473126ecbf13
Hunter <thecryptohunter@protonmail.com>
2019-06-08 15:01:32 +0100

    Update

--- search.php
+++ search.php
@@ -13,20 +13,20 @@ if (isset($_POST['address'])) {
 	} else {
 		//print_r($get_balances);
 		//echo $get_balances[$address];
-		$i = 0;
-		foreach ($get_balances as $each_member) { 
-			$i++; 
-			echo "<h2>Member $i</h2>"; 
-			while (list($keys, $value) = each ($each_member)) {                                 
-				print_r($keys);
-				print_r($value);
-				echo "<br />"; 
-			}
-		} 
+array_search($address, $get_balances);
+//		$i = 0;
+//		foreach ($get_balances as $each_member) { 
+//			$i++; 
+//			echo "<h2>Member $i</h2>"; 
+//			while (list($keys, $value) = each ($each_member)) {                                 
+//				print_r($keys);
+//				print_r($value);
+//				echo "<br />"; 
+//			}
+//		} 
 
 	}
 
-}
 
 //Check if node is online before further checks
 $check_server = $wallet->ping($scheme, $server_ip, $server_port);
