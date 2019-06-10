9cf35af050d10270a056642436de0e94a0758f5e
Hunter <thecryptohunter@protonmail.com>
2019-06-10 14:29:34 +0100

    Update

--- search.php
+++ search.php
@@ -16,11 +16,11 @@ if (isset($_POST['address'])) {
 	foreach($get_balances as $key => $value){
 		foreach($value as $a => $b){
 			foreach($b as $c => $d){
-			print_r ($b);
-			echo ($b[0]);
-			echo ($b[1]);
-			echo "<br />";
-//			if ($d == $address) {echo $d;}
+//			print_r ($b);
+//			echo ($b[0]);
+//			echo ($b[1]);
+//			echo "<br />";
+			if ($b[0] == $address) {echo $b[1];}
 			}
 		}
 	}
